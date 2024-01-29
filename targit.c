2e749e9576443a667d51ecef94b6db3ad5bbad26
m.khavari2005 <m.khavari2005@gmail.com>
2024-01-29 18:11:51 +0330

    update main.c

--- targit.c
+++ targit.c
@@ -1,9 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
+#include "init.c"
 
 int main(int argc, char *argv[]){
     if(argc<2){
         perror("The command is invalid\n");
         return 1;
     }
-    
+
 }
